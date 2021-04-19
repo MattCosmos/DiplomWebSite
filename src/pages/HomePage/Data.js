6fc878ccf44c22ce0722753b023deefa95371d40
mattcosmos <mattimaginecosmos@gmail.com>
2021-04-20 03:05:24 +0600

    init

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,68 @@
+export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Adaptive Testing',
+    headline: 'Maximizing students knowledge',
+    description:
+      'We help to identify mistakes, and then help the student to improve their knowledge in the future',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Credit Card',
+    start: ''
+  };
+  
+  export const homeObjTwo = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Instant Setup',
+    headline: 'Extremely quick onboarding process',
+    description:
+      "Once you join, our team will help you adapt to the test",
+    buttonLabel: 'Learn More',
+    imgStart: '',
+    img: require('../../images/svg-2.svg').default,
+    alt: 'Vault',
+    start: ''
+  };
+  
+  export const homeObjThree = {
+    primary: false,
+    lightBg: true,
+    lightTopLine: false,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Student Review:',
+    headline:
+      'Adaptive Testing helped me increase my education skills!',
+    description:
+      "Their team is wonderful! I can't believe I didn't start working with them earlier",
+    buttonLabel: 'View Case Study',
+    imgStart: 'start',
+    img: require('C:/Users/HundredEyes/Desktop/react-website/src/images/StudentGirl.jfif').default,
+    alt: 'Vault',
+    start: 'true'
+  };
+  
+  export const homeObjFour = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Secure Database',
+    headline: 'All your data is stored on our secure server',
+    description:
+      'You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.',
+    buttonLabel: 'Sign Up Now',
+    imgStart: 'start',
+    img: require('../../images/svg-3.svg').default,
+    alt: 'Vault',
+    start: 'true'
+  };
---
+++ src/pages/Services/Data.js
@@ -0,0 +1,55 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Access Online',
+  headline: 'Knowledge with adaptive testing',
+  description:
+    'Get knowledge in our website. You can always try and find out your level of knowledge 24/7',
+  buttonLabel: 'Get Started',
+  imgStart: '',
+  img: require('../../images/svg-4.svg').default,
+  alt: 'Credit Card'
+};
+
+export const homeObjTwo = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: '100% Secure',
+  headline: 'Stay protected 24/7 anywhere anytime',
+  description:
+    'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime',
+  buttonLabel: 'Learn More',
+  imgStart: '',
+  img: require('../../images/svg-5.svg').default,
+  alt: 'Vault'
+};
+
+export const homeObjThree = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Knowledge and Knowledge',
+  headline: 'Super fast and simple onboarding process',
+  description:
+    "The adaptive testing standard treats adaptive engines as a black box, protecting the algorithm that powers the adaptivity and defines a simple API for secure communication between systems",
+  buttonLabel: 'Start Now',
+  imgStart: 'start',
+  img: require('../../images/svg-5.svg').default,
+  alt: 'Vault'
+};
+
+export const homeObjFour = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Data Analytics',
+  headline: 'Every transaction is stored on our secure cloud database',
+  description:
+    'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
+  buttonLabel: 'Sign Up Now',
+  imgStart: 'start',
+  img: require('../../images/svg-5.svg').default,
+  alt: 'Vault'
+};
